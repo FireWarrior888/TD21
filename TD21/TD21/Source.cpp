08cc50845845d1be52124d6eefc448b783a5fbdb
FireWarrior888 <jyvesetanne@orange.fr>
2024-01-22 20:54:42 +0100

    résultat un peu plus concluant

--- TD21/TD21/Source.cpp
+++ TD21/TD21/Source.cpp
@@ -1,30 +1,124 @@
-#include "Vector2.h"
+#include <iostream>
+#include <vector>
+
+class Vector2
+{
+public:
+    float x;
+    float y;
+
+    Vector2();
+    Vector2(float _x, float _y);
+    ~Vector2();
+
+    Vector2 AddVector2f(Vector2 a, Vector2 b);
+
+    Vector2 SubstractVector2f(Vector2 a, Vector2 b);
+
+    Vector2 MultiplyVector2f(Vector2 a, float b);
+
+    Vector2 DivideVector2f(Vector2 a, float b);
+
+    Vector2 Normalize(Vector2 v);
+
+    float GetDistance(Vector2 a, Vector2 b);
+
+    float GetNorme(Vector2 a);
+
+    float GetSignedAngleBetween(Vector2 a, Vector2 b);
+
+
+private:
+
+};
 
 
 int main()
 {
-   Vector2 a = { 5,-5 };
+    Vector2 a = { 5,-5 };
     Vector2 b = { 5,5 };
     float b2 = 5;
     Vector2 v = { 8,8 };
 
     Vector2 result1 = Vector2().AddVector2f(a, b);
-    /* Vector2 result2 = Vector2().SubstractVector2f(a, b);
+    Vector2 result2 = Vector2().SubstractVector2f(a, b);
     Vector2 result3 = Vector2().MultiplyVector2f(a, b2);
     Vector2 result4 = Vector2().DivideVector2f(a, b2);
     Vector2 result5 = Vector2().Normalize(v);
     float result6 = Vector2().GetNorme(a);
     float result7 = Vector2().GetDistance(a, b);
-    float result8 = Vector2().GetSignedAngleBetween(a, b);*/
+    float result8 = Vector2().GetSignedAngleBetween(a, b);
 
 
     std::cout << "Hello World!\n";
-   std::cout << result1.x << ", " << result1.y << std::endl;
-   /*  std::cout << result2.x << ", " << result2.y << std::endl;
+    std::cout << result1.x << ", " << result1.y << std::endl;
+    std::cout << result2.x << ", " << result2.y << std::endl;
     std::cout << result3.x << ", " << result3.y << std::endl;
     std::cout << result4.x << ", " << result4.y << std::endl;
     std::cout << result5.x << ", " << result5.y << std::endl;
     std::cout << result6 << std::endl;
     std::cout << result7 << std::endl;
-    std::cout << result8 << std::endl;*/
+    std::cout << result8 << std::endl;
+}
+
+Vector2::Vector2()
+{
+}
+
+Vector2::Vector2(float _x, float _y)
+{
+    x = _x;
+    y = _y;
+}
+
+Vector2::~Vector2()
+{
+}
+
+Vector2 Vector2::AddVector2f(Vector2 a, Vector2 b)
+{
+    Vector2 addVector2 = { a.x + b.x, a.y + b.y };
+    return addVector2;
+}
+
+Vector2 Vector2::SubstractVector2f(Vector2 a, Vector2 b)
+{
+    Vector2 substractVector2 = { b.x - a.x, b.y - a.y };
+    return substractVector2;
+}
+
+Vector2 Vector2::MultiplyVector2f(Vector2 a, float b)
+{
+    Vector2 multiplyVector2 = { a.x * b, a.y * b };
+    return multiplyVector2;
+}
+
+Vector2 Vector2::DivideVector2f(Vector2 a, float b)
+{
+    Vector2 divideVector2 = { a.x / b, a.y / b };
+    return divideVector2;
+}
+
+Vector2 Vector2::Normalize(Vector2 v)
+{
+    if (GetNorme(v) != 0)
+        return DivideVector2f(v, GetNorme(v));
+    else
+        return Vector2();
+
+}
+
+float Vector2::GetDistance(Vector2 a, Vector2 b)
+{
+    return sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
+}
+
+float Vector2::GetNorme(Vector2 a)
+{
+    return sqrt(a.x * a.x + a.y * a.y);
+}
+
+float Vector2::GetSignedAngleBetween(Vector2 a, Vector2 b)
+{
+    return atan2f(a.x * b.y - a.y * b.x, a.x * b.x + a.y * b.y);
 }
--- TD21/TestUnitaireTD21/TestUnitaireTD21.cpp
+++ TD21/TestUnitaireTD21/TestUnitaireTD21.cpp
@@ -9,11 +9,10 @@ namespace TestUnitaireTD21
 	TEST_CLASS(TestUnitaireTD21)
 	{
 	public:
-
-		/*Vector2 a = { 5,-5 };
+		Vector2 a = { 5,-5 };
 		Vector2 b = { 5,5 };
 		float b2 = 5;
-		Vector2 v = { 8,8 };*/
+		Vector2 v = { 8,8 };
 
 		TEST_METHOD(TestSource)
 		{
@@ -34,40 +33,63 @@ namespace TestUnitaireTD21
 		TEST_METHOD(TestAdd)
 		{
 			Vector2 expected2 = { 10 , 0 };
-			float result2 = Vector2().AddVector2f({ 5,-5 },{5,5}).x;
+			float result2 = Vector2().AddVector2f({ 5,-5 }, { 5,5 }).x;
 			float result3 = Vector2().AddVector2f({ 5,-5 }, { 5,5 }).y;
 			Assert::AreEqual(expected2.x, result2);
 			Assert::AreEqual(expected2.y, result3);
-			
-			
+
 		}
 		TEST_METHOD(TestSubstract)
 		{
-
+			Vector2 expected3 = { 0 , 10 };
+			float result4 = Vector2().SubstractVector2f({ 5,-5 }, { 5,5 }).x;
+			float result5 = Vector2().SubstractVector2f({ 5,-5 }, { 5,5 }).y;
+			Assert::AreEqual(expected3.x, result4);
+			Assert::AreEqual(expected3.y, result5);
 		}
 		TEST_METHOD(TestMultiply)
 		{
-
+			Vector2 expected4 = { 25 , -25 };
+			float result6 = Vector2().MultiplyVector2f({ 5,-5 }, { 5 }).x;
+			float result7 = Vector2().MultiplyVector2f({ 5,-5 }, { 5 }).y;
+			Assert::AreEqual(expected4.x, result6);
+			Assert::AreEqual(expected4.y, result7);
 		}
 		TEST_METHOD(TestDivide)
 		{
-
+			Vector2 expected5 = { 1 , -1 };
+			float result8 = Vector2().DivideVector2f({ 5,-5 }, { 5 }).x;
+			float result9 = Vector2().DivideVector2f({ 5,-5 }, { 5 }).y;
+			Assert::AreEqual(expected5.x, result8);
+			Assert::AreEqual(expected5.y, result9);
 		}
 		TEST_METHOD(TestNormalize)
 		{
-
+			Vector2 expected6 = { 0.707107 , 0.707107 };
+			float result10 = Vector2().Normalize({ 8,8 }).x;
+			Assert::AreEqual(expected6.x, result10);
 		}
 		TEST_METHOD(TestNorme)
 		{
-
+			float expected7 = { 7.07107 };
+			float result11 = Vector2().GetNorme({ 5 , -5 });
+			Assert::AreEqual(expected7, result11);
 		}
 		TEST_METHOD(TestDistance)
 		{
-
+			float expected8 = { 10 };
+			float result12 = Vector2().GetDistance({ 5,-5 }, { 5,5 });
+			float result13 = Vector2().GetDistance({ 5,-5 }, { 5,5 });
+			Assert::AreEqual(expected8, result12);
+			Assert::AreEqual(expected8, result13);
 		}
 		TEST_METHOD(TestAngle)
 		{
-
+			float expected9 = { 1.5708 };
+			float result14 = Vector2().GetSignedAngleBetween({ 5,-5 }, { 5,5 });
+			float result15 = Vector2().GetSignedAngleBetween({ 5,-5 }, { 5,5 });
+			Assert::AreEqual(expected9, result14);
+			Assert::AreEqual(expected9, result15);
 		}
 	};
 }
